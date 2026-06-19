eef423969ab52e20391e0704f3539a4431834041
Naresh Samal <nareshsamal99384@gmail.com>
2026-06-19 17:22:47 +0530

    style: hide interactive hero preview card on mobile view to simplify layout and focus on core CTAs

--- src/App.tsx
+++ src/App.tsx
@@ -2053,7 +2053,7 @@ const LandingPage = () => {
                 initial={{ opacity: 0, scale: 0.95, y: 30 }}
                 animate={{ opacity: 1, scale: 1, y: 0 }}
                 transition={{ duration: 0.8, delay: 0.2 }}
-                className="flex-1 relative w-full lg:max-w-[500px]"
+                className="hidden lg:block flex-1 relative w-full lg:max-w-[500px]"
               >
                 <InteractiveHeroPreview />
               </motion.div>
